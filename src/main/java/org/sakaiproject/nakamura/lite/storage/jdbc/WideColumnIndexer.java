0ba7c2df49488e5949b2d4bde04414de6579a6b3
Ian Boston <ieb@tfd.co.uk>
2012-02-28 12:55:52 +1100

    Fixed #171, incorrect query properties were supplied to invalidate the query cache when a child was added generating an incorrect key. Also keys were being generated for all queries ignoring the cacheable flag. That would have no impact on OAE since it doesnt use SQL finder queries at all. Conflicts:
    	src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/WideColumnIndexer.java

--- src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/WideColumnIndexer.java
+++ src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/WideColumnIndexer.java
@@ -115,7 +115,8 @@ public class WideColumnIndexer extends AbstractIndexer implements CachingIndexer
                 LOGGER.debug("Hash of {}:{}:{} is {} ", new Object[] { keySpace, columnFamily,
                         parent, hash });
                 updateColumns.put(Content.PARENT_HASH_FIELD, hash);
-                invalidate(keySpace, columnFamily, ImmutableMap.of(Content.PARENT_HASH_FIELD, (Object) hash));
+                invalidate(keySpace, columnFamily, ImmutableMap.of(Content.PARENT_HASH_FIELD, (Object) hash,
+                        StorageConstants.CUSTOM_STATEMENT_SET, "listchildren", StorageConstants.CACHEABLE, true));
             }
 
             
@@ -587,34 +588,37 @@ public class WideColumnIndexer extends AbstractIndexer implements CachingIndexer
             return;
         }
         String cacheKey = getCacheKey(keySpace, columnFamily, queryProperties);
-        if ( cacheKey != null ) {
+        if (cacheKey != null) {
+            LOGGER.debug("Removing Cached Query {} ",cacheKey);
             queryCache.remove(cacheKey);
         }
     }
-    
-    private String getCacheKey(String keySpace, String columnFamily,
-            Map<String, Object> queryProperties) {
-        List<String> keys = Lists.newArrayList(queryProperties.keySet());
-        Collections.sort(keys);
-        StringBuilder sb = new StringBuilder();
-        sb.append(keySpace).append(";").append(columnFamily);
-        boolean hasKey = false;
-        for ( String key : keys ) {
-            if ( DONT_CACHE_KEYS.contains(key)) {
-                hasKey = false;
-                LOGGER.debug("Query cant be cached becuase it contains {} ",key);
-                break;
+
+    private String getCacheKey(String keySpace, String columnFamily, Map<String, Object> queryProperties) {
+        if (Boolean.parseBoolean(String.valueOf(queryProperties.get(StorageConstants.CACHEABLE)))) {
+            List<String> keys = Lists.newArrayList(queryProperties.keySet());
+            Collections.sort(keys);
+            StringBuilder sb = new StringBuilder();
+            sb.append(keySpace).append(";").append(columnFamily);
+            boolean hasKey = false;
+            for (String key : keys) {
+                if (DONT_CACHE_KEYS.contains(key)) {
+                    hasKey = false;
+                    LOGGER.debug("Query cant be cached becuase it contains {} ", key);
+                    break;
+                }
+                if (!EXCLUDE_CACHE_KEYS.contains(key)) {
+                    sb.append(key).append(":").append(queryProperties.get(key)).append(";");
+                    hasKey = true;
+                }
             }
-            if ( !EXCLUDE_CACHE_KEYS.contains(key) ) {
-                sb.append(key).append(":").append(queryProperties.get(key)).append(";");
-                hasKey = true;
+            if (hasKey) {
+                // we might want to hash this to prevent the key getting
+                // massive.
+                LOGGER.debug("Cached Query Key is {} ", sb.toString());
+                return sb.toString();
             }
         }
-        if ( hasKey ) {
-            // we might want to hash this to prevent the key getting massive.
-            return sb.toString();
-        }
-        return null;
     }
 
     private DisposableIterator<Map<String, Object>> cachedFind(String keySpace,
@@ -632,11 +636,13 @@ public class WideColumnIndexer extends AbstractIndexer implements CachingIndexer
             CacheHolder ch = queryCache.get(cacheKey);
             if ( ch != null ) {
                 Map<String, Object> m = ch.get();
-                if ( m != null ) {
+                if (m != null) {
+                    LOGGER.debug("Got Cached Query at {} ",cacheKey);
                     return new PreemptiveCachedMapIterator(client, keySpace, columnFamily, m, rawResults, cachingManager);
                 }
             }
         }
+        LOGGER.debug("No Cached Query at {} ",cacheKey);
         return null;
     }
     
@@ -652,7 +658,8 @@ public class WideColumnIndexer extends AbstractIndexer implements CachingIndexer
         String cacheKey = getCacheKey(keySpace, columnFamily, queryProperties);
         if ( cacheKey != null ) {
             Map<String, Object> m = disposableIterator.getResultsMap();
-            if ( m != null ) {
+            if (m != null) {
+                LOGGER.debug("Saved Cached Results at {} ",cacheKey);
                 queryCache.put(cacheKey, new CacheHolder(m));
             }
         }
